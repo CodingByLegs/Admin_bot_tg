46eb7d55afe1c5bb88f0fc86507df3c509ecf8f0
James-Bolt <gleb.frolow2013@gmail.com>
2021-11-20 15:51:28 +0300

    Add BotToken

---
+++ keyboards/inline/InlineKeyBoard.py
@@ -0,0 +1,7 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
+from loader import bot
+from loader import dp
+
+
+#async def creat_categoryes_kb():
