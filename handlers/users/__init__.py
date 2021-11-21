3e0e2677efca0a5970c6895cf9cd5d9a1ea4cd3c
James-Bolt <gleb.frolow2013@gmail.com>
2021-11-21 20:26:32 +0300

    Add refresh_categories_kb

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,4 +2,4 @@ from . import help
 from . import start
 from . import echo
 from . import StartMenu
-
+from . import Callback_query_handlers
--- keyboards/inline/callback_dates.py
+++ keyboards/inline/callback_dates.py
@@ -1,3 +1,4 @@
 from aiogram.utils.callback_data import CallbackData
 
-category_callback = CallbackData("category", "category_name", "page", "page_number", "rotation", "action_name")
+category_callback = CallbackData("category", "category_name", "page")
+page_callback = CallbackData("page", "page_number", "rotation")
